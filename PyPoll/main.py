5309b96c276a57fcc8bfd6a76610dc455bd1e17c
Debolina <bhaumikdebolina@gmail.com>
2022-04-04 19:18:01 -0500

    adding the changes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,32 +4,35 @@ import csv
 csvpath = os.path.join("Resources", "election_data.csv")
 print("Election Results")
 print("__________________")
-votes = []
-candidates = []
+#votes = []
+#candidates = []
+electionData = []
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print (csvreader)
+    #print (csvreader)
     header = next(csvreader)
 
     ## Initialize variables
-    votes_cast = 0
-    list_of_candidates = set()
-    vote_Charles_Casper = 0
-    vote_Diana_DeGette = 0
-    vote_Raymon_Anthony = 0
+    #votes_cast = 0
+    #list_of_candidates = set()
+    #vote_Charles_Casper = 0
+    #vote_Diana_DeGette = 0
+    #vote_Raymon_Anthony = 0
 
     for column in csvreader:
-        votes.append(column[0])
-        candidates.append(column[2])
+        #votes.append(column[0])
+        #candidates.append(column[2])
+        electionData.append(column)
 
-    Total_Votes = (len(votes))
+    Total_Votes = (len(electionData))
     print(f"Total Votes: {Total_Votes}")
 
     #count candidates
-    Charles = int(candidates.count("Charles"))
-    Diana = int(candidates.count("Diana"))
-    Raymon = int(candidates.count("Raymon"))
-
+    Charles = len([i for i in electionData if i[2] == "Charles Casper Stockham"])
+    Diana = len([i for i in electionData if i[2] == "Diana DeGette"])
+    Raymon = len([i for i in electionData if i[2] == "Raymon Anthony Doane"])
+   # total_candidates = len(set([i[2] for i in electionData]))
+    
     #percentage calculation
     Charles_percentage = (Charles/Total_Votes) * 100
     Diana_percentage = (Diana/Total_Votes) * 100
@@ -39,3 +42,12 @@ with open(csvpath, 'r') as csvfile:
     print(f"Charles: {Charles_percentage}% ({Charles})")
     print(f"Diana: {Diana_percentage}% ({Diana})")
     print(f"Raymon: {Raymon_percentage}% ({Raymon})")
+
+    #choose winner
+    if Charles > Diana > Raymon:
+        Winner = "Charles"
+    elif Diana > Charles > Raymon:
+        Winner = "Diana"
+    elif Raymon > Charles > Diana:
+        Winner = "Raymon"
+    print (f"Winner: {Winner}")
